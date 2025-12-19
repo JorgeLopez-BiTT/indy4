c55931f74449ee0c18f921288a614d8e1c9108dc
Jorge.Signus <cexetor@gmail.com>
2025-12-19 12:43:48 -0600

    integrate allure e2e v4 trace on

--- playwright.config.ts
+++ playwright.config.ts
@@ -42,7 +42,7 @@ const config: PlaywrightTestConfig = {
     baseURL: 'https://practice.sdetunicorns.com',
 
     /* Collect trace when retrying the failed test. See https://playwright.dev/docs/trace-viewer */
-    trace: 'retain-on-failure',
+    trace: 'on',
 
     storageState: 'loggedInState.json'
   },
